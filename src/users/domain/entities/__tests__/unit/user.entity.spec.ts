a85d330d4d30f5c6fcecd62ae6cdb93de3f8cb93
jotavetech <ojoaogugel@gmail.com>
2024-07-07 23:20:32 -0300

    helper: user data builder to unit tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,17 +1,13 @@
-import { faker } from '@faker-js/faker';
-
 import { UserEntity, UserProps } from '../../user.entity';
+
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
 
 describe('UserEntity unit tests', () => {
   let props: UserProps;
   let user: UserEntity;
 
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
+    props = UserDataBuilder({});
 
     user = UserEntity.create(props);
   });
